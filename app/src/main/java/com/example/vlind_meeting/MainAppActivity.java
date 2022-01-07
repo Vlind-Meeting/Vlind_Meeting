36eb15c029fe499532ec9dc142949f678d32e174
Hans-0101 <hyhsumin@naver.com>
2022-01-08 05:03:21 +0900

    feat:matching page

--- app/src/main/java/com/example/vlind_meeting/MainAppActivity.java
+++ app/src/main/java/com/example/vlind_meeting/MainAppActivity.java
@@ -5,16 +5,29 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
+import me.relex.circleindicator.CircleIndicator3;
+
 public class MainAppActivity extends AppCompatActivity {
+
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 2;
+    private CircleIndicator3 mIndicator;
+    private CheckBox select_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,42 @@ public class MainAppActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //본인의 프로필을 보여줘야 함
         //그리고 여기서 이성한테 하트를 보내는 창, 메시지 함 등으로 넘어가는 아이콘들을 구현해야함. 그리고 추가로 Activity들을 생성해야 한다.
+
+
+        //ViewPager2
+        mPager = findViewById(R.id.viewpager);
+        //Adapter
+        pagerAdapter = new AdapterMainAppActiviy(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+        //Indicator
+        mIndicator = findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+        mIndicator.createIndicators(num_page,0);
+        //ViewPager Setting
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+
+        mPager.setCurrentItem(0); //시작 지점
+        mPager.setOffscreenPageLimit(4); //최대 이미지 수
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if (positionOffsetPixels == 0) {
+                    mPager.setCurrentItem(position);
+                }
+            }
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mIndicator.animatePageSelected(position%num_page);
+            }
+        });
+
+        select_btn = (CheckBox) findViewById(R.id.select_btn);
+        if(select_btn.isChecked()){
+            //getCurrentItem에 따라서 데이터베이스에서 받아오기
+            Toast.makeText(getApplicationContext(), String.valueOf(mPager.getCurrentItem()), Toast.LENGTH_SHORT).show();
+        }
     }
 
 
---
+++ app/src/main/java/com/example/vlind_meeting/Match1Fragment.java
@@ -0,0 +1,20 @@
+package com.example.vlind_meeting;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Match1Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_match1, container, false);
+        return rootView;
+    }
+
+
+}

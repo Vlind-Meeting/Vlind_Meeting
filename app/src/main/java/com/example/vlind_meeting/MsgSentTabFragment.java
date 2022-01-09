4adc7e884353537ca5b429b50c5ca8f777c63bf3
Hans-0101 <hyhsumin@naver.com>
2022-01-09 21:39:54 +0900

    feat:editing msg sent page

--- app/src/main/java/com/example/vlind_meeting/MsgSentTabFragment.java
+++ app/src/main/java/com/example/vlind_meeting/MsgSentTabFragment.java
@@ -39,9 +39,9 @@ public class MsgSentTabFragment extends Fragment {
         mList = new ArrayList<>();
         for(int i=1;i<=10;i++){
             if(i%2==0)
-                mList.add(new MsgSentRecyclerItem(R.drawable.heart_filled,"s","수락 대기 중"));
+                mList.add(new MsgSentRecyclerItem(R.drawable.heart1,"sumin","수락 대기 중"));
             else
-                mList.add(new MsgSentRecyclerItem(R.drawable.heart_unfilled,"i","수락 대기 중"));
+                mList.add(new MsgSentRecyclerItem(R.drawable.heart2,"핸스","수락 대기 중"));
         }
         mRecyclerViewAdapter.setFriendList(mList);
 
